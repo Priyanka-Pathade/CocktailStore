39b079827fb348cfc5f7c194f3b557e829896880
PRIYANKA PATHADE <priyankapathade4773@gmail.com>
2022-11-09 22:25:50 +0530

    changes added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import SingleCocktail from './Pages/SingleCocktail';
+import Error from './Pages/Error';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar />
+    
+    </>
+    // <Router >
+    // <p>hii</p>
+    // //   <Navbar />
+    // //   <Routes>
+    // //     <Route path='/' element={<Home />}/>
+    // //     <Route path='about' element={<About />} />
+    // //     <Route path='cocktail/:id' element={<SingleCocktail />} />
+    // //     <Route path='*' element={<Error />} />
+    // //   </Routes>
+    //  </Router>
   );
 }
 
